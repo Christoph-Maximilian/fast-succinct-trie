78d779da2534c595961e1f975d1c4da5d1e5be77
Christoph Anneser <christop.anneser@tum.de>
2019-04-08 23:50:59 +0200

    correct data type for sbits to work with millions of keys

--- fst/src/FST.cpp
+++ fst/src/FST.cpp
@@ -762,6 +762,7 @@ bool FST::lookup(const uint8_t *key, const int keylen, uint64_t &value) {
             return true;
         }
 
+
         pos = childpos(childNodeNum(pos) + childCountU_);
         keypos++;
 
